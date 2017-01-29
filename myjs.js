022801dba71c585574acbcfb45547695a6b797c0
kevin-mylett <kevinmylett@sky.com>
2017-01-29 21:12:45 +0000

    Using pure JS instead of jQuery

--- myjs.js
+++ myjs.js
@@ -1,10 +1,23 @@
-$(document).ready(function() {
-	
-	$(".nav-toggle").click(function() {
-
-		$(".site-navigation ul").toggleClass("open");
-		$(".nav-toggle").toggleClass("open");
-
-	});
-
+document.addEventListener('DOMContentLoaded', function() {
+    smallScreenMenu();
 });
+
+function smallScreenMenu() {
+
+	var nav = document.getElementsByClassName('nav-toggle');
+	var slideNav = nav[0];
+
+	slideNav.onclick = function() {
+
+		var menu = document.getElementsByClassName('menu');
+		var slideNavMenu = menu[0];
+
+		if (slideNavMenu.className == 'menu') {
+			slideNavMenu.className += ' open';
+			slideNav.className += ' open';
+		} else {
+			slideNavMenu .className = 'menu';
+			slideNav.className = 'nav-toggle';
+		}
+	}
+};
